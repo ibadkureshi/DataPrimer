33184edb091f900a10ac2dfbe10cc97898a4725d
ibadkureshi <ibn.kureshi@gmail.com>
2016-06-28 16:57:00 +0100

    made the data.py dynamic
    added dynamism to the data.py

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
 import sys
 
 f=open("data.csv","r")
+analyse=sys.argv[1]
+
 runningTotal=0
 runningPaid=0
 runningBalance=0
@@ -20,12 +22,31 @@ print "Total Cost = " + str(runningTotal)
 print "Total Paid = " + str(runningPaid)
 print "Total Balance = " + str(runningBalance)
 
+
 f.seek(0)
+counter=0
+runningTotal=0
+runningPaid=0
+runningBalance=0
+
 for line in f:
 	line=line.strip('\n').strip('\r')
 	entry=line.split(',')
-	if entry[5]=="pending":
-		print "Clients " + entry[1] + " are pending"
+	if entry[5]==analyse:
+		counter+=1
+		print "Clients " + entry[1] + " are " + analyse
+		try:
+			runningTotal += int(entry[2])
+			runningPaid += int(entry[3])
+			runningBalance += int(entry[4])
+		except:
+			pass
+print str(counter) + " entries are " + analyse
+print analyse + " Cost = " + str(runningTotal) 
+print analyse + " Paid = " + str(runningPaid)
+print analyse + " Balance = " + str(runningBalance)
+
+
 
 f.close
 
